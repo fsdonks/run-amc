08bfac2052747f825444687f97abdf3de41b12ea
Smith <1522119730121002@mil>
2022-10-31 14:12:06 -0400

    Multiple inputs, minimum risk

--- edta_risk_chart/edta_risk.R
+++ edta_risk_chart/edta_risk.R
@@ -1,6 +1,8 @@
 library("dplyr")
 library("ggplot2")
 library(purrr)
+library(readxl)
+library(writexl)
 
 out_location="./workspace/run-amc/outputs/"
 
@@ -21,19 +23,37 @@ ac_rc <- function(data_initial){
   data
 }
 
+# #searches for matching filename pattern in working directory
+# inputfiles <- 
+#   list.files(pattern="*Initial*")
+# 
+# #reads-in matched file
+# data_initial <-
+#   as.data.frame(read_excel(inputfiles, trim_ws = TRUE, col_names = TRUE))
+
 data_initial <-
   as.data.frame(read_excel("./workspace/run-amc/edta_risk_chart/Data Initial List.xlsx", trim_ws = TRUE, col_names = TRUE))
 
 split_data <- 
   split(data_initial, f = sort(as.numeric(rownames(data_initial))))
 
-data <- map_dfr(split_data, ac_rc)
+data <- 
+  map_dfr(split_data, ac_rc)
 
-supply <- data %>% 
+supply <- 
+  data %>% 
   mutate("T0A"=RA*1.0, "T2A"=ifelse(0.2>RC_Available, RA+RC*RC_Available, RA+RC*0.2), "T3A"=RA+RC*RC_Available)
+
+# #searches for matching filename pattern in working directory
+# inputdemands <- 
+#   list.files(pattern="*demand*")
+# 
+# #reads-in matched filenames - currently a csv
+# demand <- as.data.frame(read.csv(inputdemands))
 
 demand <- read.csv("./workspace/run-amc/edta_risk_chart/early_demand.csv", stringsAsFactors=FALSE)
 
+#computes the score and risk for the SRCs
 data_fill <- left_join(supply, demand, by="SRC") %>%
   mutate("available"=case_when(Day<T2 ~ T0A, Day<T3 ~ T2A, Day>=T3 ~ T3A)) %>%
   mutate("fill"=ifelse(available>Demand, Demand, available)) %>%
@@ -46,9 +66,12 @@ data_fill <- left_join(supply, demand, by="SRC") %>%
                           fill_rate>=0.70 ~ 2,
                           fill_rate>=0 ~ 1))
 
+# #writes the dataframe to an Excel file
+# write_xlsx(data, "test_data.xlsx")
+
 write.table(data_fill, paste(out_location, "edta_output.txt"), sep="\t", row.names=FALSE)
 
-risk_colors=c("#F05454", "#FFC990", "#FFF990", "#3DCC91", "#005000")
+risk_colors=c("#F05454", "#FFC990", "#FFF990", "#3DCC91", "#009E73")
 risk_labels=c("extreme", "major", "modest", "minor", "none")
 
 for(src in unique(data_fill$SRC)){
@@ -58,15 +81,16 @@ for(src in unique(data_fill$SRC)){
   labels_subset=risk_labels[risks]
   
   g <- ggplot(src_data, aes(RA, RC, fill=Risk)) + 
-  geom_tile(color="black") +
-  scale_fill_gradientn(colors=colors_subset,
-                       labels=labels_subset, breaks=risks) +
-  ggtitle(src) +
-  theme_bw() +
-  theme(text=element_text(size=20),
-        axis.text=element_text(size=20),
-        legend.text=element_text(size=20))
+    geom_tile(color="black", position = position_nudge(x = 0.5, y = 0.5)) +
+    scale_fill_gradientn(colors=colors_subset,
+                         labels=labels_subset, breaks=risks) +
+    #ggtitle(src) +
+    theme_bw() +
+    theme(text=element_text(size=20),
+          axis.text=element_text(size=20),
+          legend.text=element_text(size=20),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank())
   print(g)
   ggsave(paste(out_location, "edta_", src, ".png", sep=""))
 }
-                       
--- taa_risk_chart/taa_risk_chart.R
+++ taa_risk_chart/taa_risk_chart.R
@@ -1,12 +1,44 @@
 library("dplyr")
 library("ggplot2")
 library("tidyr")
+library(writexl)
+library(readxl)
 
 out_location="./workspace/run-amc/outputs/"
-input <- read.csv("./workspace/run-amc/taa_risk_chart/results_archive.csv", stringsAsFactors=FALSE)
-input$phase <- gsub("-", "", input$phase)
 
-data <- input %>%
+inputfiles <- 
+  list.files(pattern="results_*")
+
+#Prep input files for separation; capture filenames
+names <- substr(inputfiles, 9, 13)
+all_files <- lapply(inputfiles, read.csv)
+names(all_files) <- substr(inputfiles, 9, 13)
+
+i <- as.numeric(1)
+n <- as.numeric(length(names))
+
+#Create risk columns; cleanup phase cols
+while(i <= n) {
+  
+  Risk <- rep(i, nrow(all_files[[i]]))
+  all_files[[i]][ , ncol(all_files[[i]]) + 1] <- Risk
+  colnames(all_files[[i]])[ncol(all_files[[i]])] <- paste0("Risk")
+  
+  all_files[[i]]$phase <- gsub("-", "", all_files[[i]]$phase)
+  
+  i <- i+1
+  
+}
+rm(Risk)
+
+#This function can split the list of dataframes, if needed
+#list2env(all_files, envir=.GlobalEnv)
+
+#This function computes the scores and risks for each SRC's AC-RC permutation 
+functor <- 
+  function(input){
+    
+  input %>%
   rename(RA=AC) %>%
   mutate(fill=AC.fill+RC.fill, demand=total.quantity) %>%
   mutate(fill=ifelse(RA+RC==0, 0, fill), demand=ifelse(RA+RC==0, 1, demand)) %>%
@@ -22,10 +54,39 @@ data <- input %>%
                           score>=0.80 ~ 3,
                           score>=0.70 ~ 2,
                           score>=0 ~ 1))
+}
+
+#Loop through list of dataframes, applying function to each item
+i <- as.numeric(1)
+
+while(i <= n) {
+  all_files[[i]] <- functor(all_files[[i]])
+  i <- i+1
+}
+
+#Reduce list of dataframes to single object
+data <- 
+  bind_rows(all_files, .id = "column_label")
+
+data$risk_min <- NA
+
+#Determine the min risk per SRC-AC-RC group; save in risk_min
+data <-
+  data %>%
+  group_by(SRC, RA, RC) %>%
+  mutate(risk_min = min(Risk)) %>%
+  ungroup()
+
+#Ensure that only the min values are used for plotting
+data <- 
+  subset(data, Risk == risk_min)
+
+# #write dataframe to Excel file
+# write_xlsx(data, "test_data.xlsx")
 
 write.table(data, paste(out_location, "taa-risk_output.txt"), sep="\t", row.names=FALSE)
 
-risk_colors=c("#F05454", "#FFC990", "#FFF990", "#3DCC91", "#005000")
+risk_colors=c("#F05454", "#FFC990", "#FFF990", "#3DCC91", "#009E73")
 risk_labels=c("extreme", "major", "modest", "minor", "none")
 
 for(src in unique(data$SRC)){
@@ -38,25 +99,27 @@ for(src in unique(data$SRC)){
   colors_subset=risk_colors[risks]
   labels_subset=risk_labels[risks]
   g <- ggplot(src_data, aes(RA, RC, fill=Risk)) + 
-        geom_tile(color="black") +
-        ggtitle(src) +
-        scale_fill_gradientn(colors=colors_subset,
-                             labels=labels_subset,
-                             breaks=risks) +
-        theme_bw() +
-        theme(text=element_text(size=20),
-              axis.text=element_text(size=20),
-              legend.text=element_text(size=20))
-    print(g)
-    ggsave(paste(out_location, "taa_", src, ".png", sep=""))
+    geom_tile(color="black", position = position_nudge(x = 0.5, y = 0.5)) +
+    #ggtitle(src) +
+    scale_fill_gradientn(colors=colors_subset,
+                         labels=labels_subset,
+                         breaks=risks) +
+    theme_bw() +
+    theme(text=element_text(size=20),
+          axis.text=element_text(size=20),
+          legend.text=element_text(size=20),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank())
+  print(g)
+  ggsave(paste(out_location, "taa_", src, ".png", sep=""))
 }
 
 #continuous color scale, but John doesn't like this as much as discrete colors
 a<-ggplot(filter(data, SRC=="87312K000"), aes(RA, RC, fill=score)) + 
   geom_tile(color="black") +
   scale_fill_gradient(low = "red",
-                        high = "green")+
-
+                      high = "green")+
+  
   theme_bw() +
   theme(text=element_text(size=20),
         axis.text=element_text(size=20),
