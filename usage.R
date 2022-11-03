ee238e88f14bcdd9e4ee502ad17fb970443ab235
craig <craig.flewelling@gmail.com>
2022-11-03 13:29:10 +0000

    Users should only need to edit my_runamc_folder on each machine.

--- usage.R
+++ usage.R
@@ -1,12 +1,20 @@
-source(paste(getwd(), "/workspace/run-amc/edta_risk_chart/edta_risk.R", sep=''))
-source(paste(getwd(), "/workspace/run-amc/taa_risk_chart/taa_risk_chart.R", sep=''))
+my_runamc_folder=paste(getwd(), "/workspace/run-amc/", sep='')
+#load code from the two runamc scripts
+source(paste(my_runamc_folder, "edta_risk_chart/edta_risk.R", sep=''))
+source(paste(my_runamc_folder, "taa_risk_chart/taa_risk_chart.R", sep=''))
 
-make_edta_charts("./workspace/run-amc/edta_risk_chart/Data Initial List.xlsx",
-                 "./workspace/run-amc/edta_risk_chart/early_demand.csv",
-                 "./workspace/run-amc/outputs/")
+#make the edta risk charts
+edta_supply=paste(my_runamc_folder, "edta_risk_chart/Data Initial List.xlsx", sep='')
+edta_demand=paste(my_runamc_folder, "edta_risk_chart/early_demand.csv", sep='')
+output_path=paste(my_runamc_folder, "outputs/", sep='')
+make_edta_charts(edta_supply,
+                 edta_demand,
+                 output_path)
 
+#make the taa risk charts
 weights<- c("comp"=0.25, "phase1"=0.75)
-
-results_files=list("./workspace/run-amc/taa_risk_chart/results_test.csv",
-                   "./workspace/run-amc/taa_risk_chart/results_archive.csv")
-make_taa_charts("./workspace/run-amc/outputs/", results_files, weights)
+m4_results_1=paste(my_runamc_folder, "taa_risk_chart/results_test.txt", sep='')
+m4_results_2=paste(my_runamc_folder, "taa_risk_chart/results_archive.txt", sep='')
+results_files=list(m4_results_1,
+                   m4_results_2)
+make_taa_charts(output_path, results_files, weights)
