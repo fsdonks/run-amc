56c4f155ec83dd83f057d40b645b9dd132fafb07
craig <craig.flewelling@gmail.com>
2023-02-08 15:30:51 +0000

    AC and RC random run capability moved to marathon.analysis.random

--- ac-rc_random-runs/usage.clj
+++ ac-rc_random-runs/usage.clj
@@ -1,15 +1,15 @@
-(load-file "/home/craig/workspace/run-amc/ac-rc_random-runs/runamc.clj")
-(ns marathon.analysis.runamc)
+(ns marathon.analysis.random)
 ;;example usage
 ;;m4 workbook should have all NG and RC supply coded as being RC
 (def path "/home/craig/workspace/run-amc/ac-rc_random-runs/testdata.xlsx")
 (def proj (a/load-project path))
 (def phases [["comp" 1 821] ["phase-1" 822 967]])
 
-(def results (r/rand-runs-ac-rc 5 ;;min-distance
+(def results (rand-runs-ac-rc 5 ;;min-distance
                                 0.5 ;;lower-rc
                                 0.7 ;;upper-rc
-                              proj :reps 2 :phases phases
+                              (add-transform proj adjust-cannibals
+                                []) :reps 2 :phases phases
                               :lower 0 :upper 0.1
                               :compo-lengths r/default-compo-lengths
                               ))
